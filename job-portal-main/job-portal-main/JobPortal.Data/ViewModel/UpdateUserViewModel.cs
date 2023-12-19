672adf6dd9853d23063225c7c70da293c5861b1a
nphnamm <nphnamm@gmail.com>
2023-12-19 15:04:48 +0700

    first commit

---
+++ job-portal-main/job-portal-main/JobPortal.Data/ViewModel/UpdateUserViewModel.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace JobPortal.Data.ViewModel
+{
+    public class UpdateUserViewModel
+    {
+        [EmailAddress]
+        [Display(Name = "Email account login")]
+        public string? Email { get; set; }
+
+        [Display(Name = "Company name")]
+        [StringLength(100, ErrorMessage = "Company name cannot be more than 100 characters.")]
+        public string? FullName { get; set; }
+        [Display(Name = "Company overview")]
+        public string? Description { get; set; }
+        [Display(Name = "Company contact")]
+   
+        public IFormFile? UrlAvatar { get; set; }
+        [Display(Name = "Location")]
+ 
+
+    
+        public string? Phone { get; set; }
+        public int? Age { get; set; }
+
+
+
+
+    }
+}
